fb9f1ca47cd027199d68289907c6239ffd243f2a
Uverlink <marc.pizzanelli@gmail.com>
2022-11-10 09:37:08 +0100

    Setup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
+app.get('/hello', (req, res) => res.send('Hello Marc'));
 
-app.listen(3000, ()=> console.log("Application démarrée sur le port 3000"));
+app.listen(3008, ()=> console.log("Application démarrée sur le port 3008"));
