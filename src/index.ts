ecccbd8b7395ad02910a5cfdf74b84f560c1e8ea
Uverlink <marc.pizzanelli@gmail.com>
2022-11-10 10:16:41 +0100

    ?2

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Marc'));
 
-app.listen(3008, ()=> console.log("Application démarrée sur le port 3008"));
+app.listen(3008, ()=> console.log("Application démarrée sur le port 3008?"));
