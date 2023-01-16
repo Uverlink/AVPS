4c7b611d9393330e2a56c028cfb763750c92bae8
Uverlink <marc.pizzanelli@gmail.com>
2023-01-16 09:50:02 +0100

    Docker Test 3

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ const app = express()
 
 app.get('/hello', (req, res) => res.send('Hello Marc'));
 
-app.listen(3017, ()=> console.log("Application démarrée sur le port 3017?")); 
+app.listen(3017, ()=> console.log("Application démarrée sur le port 3017?"));
 
 app.get('/docker', (req, res) => res.send("J'adore cet " + process.env.test));
